a127804588ab162dd9fceff8b13f0634e923c0b3
mxavier91 <mxavier17@gmail.com>
2018-05-07 14:31:56 -0400

    Created different views
    Created index, show, and create views. Need to finish up on update and
    delete views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,49 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
 def new
+  @post = Post.new
 end
 
 def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
+  end
+end
+
+def show
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+
+
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+
+private
+
+def post_params
+  # Somewhat of a security feature in rails. Onlys allows what is initalized to be sent to the server and to the form.
+  params.require(:post).permit(:title, :content)
 end
 
 end
