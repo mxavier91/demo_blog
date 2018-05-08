3ed9f0599c042c2c3a7b86da5236cd1c47b0c662
mxavier91 <mxavier17@gmail.com>
2018-05-08 15:24:07 -0400

    Added some comments to better understand code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ def new
   @post = Post.new
 end
 
+
 def create
   @post = Post.new(post_params)
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 # The model is responsible for the data of or post and interacting with the database
+
+# Models are Ruby classes. They talk to the database, store, and validate data, perform the business logic and otherwise do the heavy lifting.
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  # dependent: :destroy means the comments related
+  # to the specific post in mention get deleted if the post does
 end
